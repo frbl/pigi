d7aaa7e964a3e612a2e5d8c324fd6422000cb28d
Rene <renezuidhof@hotmail.com>
2012-09-23 18:22:51 +0200

    Retreiving revisions update

--- Client/src/main/java/nl/rug/client/App.java
+++ Client/src/main/java/nl/rug/client/App.java
@@ -21,7 +21,12 @@ import javax.swing.UnsupportedLookAndFeelException;
 import nl.rug.client.gui.MainWindow;
 import org.tmatesoft.svn.core.SVNDirEntry;
 import org.tmatesoft.svn.core.SVNNodeKind;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 
 /**
@@ -31,8 +36,57 @@ import org.tmatesoft.svn.core.io.SVNRepository;
 public class App {
 
     public static void main(String[] args) {
-        /*DAVRepositoryFactory.setup();
-        String url = "https://subversion.assembla.com/svn/ReneZ/";
+               
+        // These need to be done to make JOGL and SQLite4Java work correctly (Maven/JNI)
+        JOGLWrapper.init();
+        SQLite.setLibraryPath("target/lib/");
+
+
+        //TODO Move database stuff away from here, remember to let the database connection run on its own thread as described in the sqlite4java getting started page
+        File database = new File("client.db");
+
+        SQLiteConnection db = new SQLiteConnection(database);
+        try {
+            db.open(true);
+            // for some reason creating the table from the terminal makes it unreadable: [file is encrypted or is not a database] (?)
+            db.exec("CREATE TABLE orders (order_id INTEGER PRIMARY KEY, quantity INTEGER)");
+        } catch (SQLiteException e) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Could not connect to the database", e);
+        }
+
+        SQLiteStatement st = null;
+
+        try {
+            st = db.prepare("SELECT order_id FROM orders WHERE quantity >= ?");
+            // first param replaces the '?' in the statement with the value of the second param
+            st.bind(1, 1);
+            while (st.step()) {
+                System.out.println(st.columnLong(0));
+            }
+
+        } catch (SQLiteException e) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Error while executing query", e);
+        } finally {
+
+            if (st != null) {
+                st.dispose();
+            }
+
+        }
+
+        db.dispose();
+
+        initializeGUI();
+
+        retrieveSVNRepository();
+        
+    }
+    
+    public static void retrieveSVNRepository(){
+        DAVRepositoryFactory.setup();
+        String url = "https://subversion.assembla.com/svn/ReneZ";
+        String path = "src/view/";
+        //String url = "http://svn.wxwidgets.org/svn/wx/wxWidgets/";
         String name = "anonymous";
         String password = "anonymous";
 
@@ -54,65 +108,31 @@ public class App {
             }
             
             for(int i = 1; i <= repository.getLatestRevision(); i++){
-                listEntries(repository, i, "");
+                listEntries(repository, i, path);
+                System.out.println("Retrieved revision " + i + " of " + path);
             }
 
         } catch (Exception ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-*/
-        // These need to be done to make JOGL and SQLite4Java work correctly (Maven/JNI)
-        JOGLWrapper.init();
-        SQLite.setLibraryPath("target/lib/");
-
-
-        //TODO Move database stuff away from here, remember to let the database connection run on its own thread as described in the sqlite4java getting started page
-        File database = new File("client.db");
-
-        SQLiteConnection db = new SQLiteConnection(database);
-        try {
-            db.open(true);
-            // for some reason creating the table from the terminal makes it unreadable: [file is encrypted or is not a database] (?)
-            db.exec("CREATE TABLE orders (order_id INTEGER PRIMARY KEY, quantity INTEGER)");
-        } catch (SQLiteException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Could not connect to the database", ex);
         }
-
-        SQLiteStatement st = null;
-
-        try {
-            st = db.prepare("SELECT order_id FROM orders WHERE quantity >= ?");
-            // first param replaces the '?' in the statement with the value of the second param
-            st.bind(1, 1);
-            while (st.step()) {
-                System.out.println(st.columnLong(0));
-            }
-
-        } catch (SQLiteException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Error while executing query", ex);
-        } finally {
-
-            if (st != null) {
-                st.dispose();
-            }
-
-        }
-
-        db.dispose();
-
-        initializeGUI();
-
     }
 
+    private static int counter = 0;
+    
     public static void listEntries(SVNRepository repository, int revision, String path) throws Exception {
-        Collection entries = repository.getDir(path, revision, null, (Collection) null);
+        if(repository.checkPath(path, revision) == SVNNodeKind.NONE){
+            return;
+        }
+        System.out.println("counter: " + counter++ + " path: " + path + " revision: " + revision);
+        System.out.println("check: " + repository.checkPath(path, revision));
+        Collection entries = repository.getDir(repository.getRepositoryPath(path), revision, null, (Collection) null);
         Iterator iterator = entries.iterator();
         String localpath = "svnfiles\\ReneZ\\" + revision + "\\" + path;
         File svnpath = new File(localpath);
         svnpath.mkdirs();
-        
         while (iterator.hasNext()) {
             SVNDirEntry entry = (SVNDirEntry) iterator.next();
-            String filePath = (path.equals("") ? "" : path + "/") + entry.getName();
+            String filePath = path + entry.getName();
             System.out.println("/" + filePath
                     + " ( author: '" + entry.getAuthor() + "'; revision: " + entry.getRevision()
                     + "; date: " + entry.getDate() + ")");
@@ -123,7 +143,7 @@ public class App {
                 localsvnfile.createNewFile();
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 OutputStream outputStream = new FileOutputStream(localsvnfile);
-                repository.getFile(entry.getName(), entry.getRevision(), new HashMap(), baos);
+                repository.getFile(filePath, entry.getRevision(), new HashMap(), baos);
                 baos.writeTo(outputStream);
             }
         }
