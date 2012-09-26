0e57434db22fc1da9db3189e3132759d9993729f
Frank Blaauw <frank.blaauw@gmail.com>
2012-09-26 23:31:01 +0200

    Added a simple graph and updates the graph using commet. Note that the graph needs to be updated to googles new API.

--- pigi/src/main/scala/nl/rug/comet/UpdateStatistics.scala
+++ pigi/src/main/scala/nl/rug/comet/UpdateStatistics.scala
@@ -7,21 +7,48 @@ import net.liftweb.util._
 import net.liftweb.http.js.JsCmds.SetHtml
 import scala.xml.Text
 import net.liftweb._
+import scala.util.Random
 
 case object Tick
+
+class Entry(pname: String, purl: String, pcomplexity: Int) {
+	
+	var name = pname;
+	var url = purl;
+	var complexity = pcomplexity;
+	
+}
 
 class UpdateStatistics extends CometActor {
   Schedule.schedule(this, Tick, 1 seconds)
 	
+	val r = new Random()
+	
+	var entries = List(
+		new Entry("WebKit","svn://webkit.com/root", 3),
+		new Entry("wxWidgets","svn://wxWidgets.org/", 8),
+		new Entry("ReneZ","svn://assembla/sds", 92)
+	).zipWithIndex
+	
   def render = {
-		println("render")
-		"#time *" #> timeNow.toString
+	
+		val image = ("https://chart.googleapis.com/chart?cht=p3&chd=t:"+r.nextInt(40)+","+r.nextInt(60)+","+r.nextInt(30)+"&chs=250x100&chl=WebKit|wxWidgets|ReneZ");
+		
+		".instance *" #> entries.map { case(entry, id) => {
+        ".index *" #> id &
+				".name *" #> entry.name &
+        ".url *"  #> entry.url &
+				".complexity *" #> entry.complexity
+      }
+    } &
+		"#time *" #> timeNow.toString &
+		"#chart [src]" #> image
 	}
 
   override def highPriority = {
     case Tick =>
 			println("tick");
-			
+			reRender(true)
 			partialUpdate(SetHtml("time", Text(timeNow.toString)))
       Schedule.schedule(this, Tick, 1 seconds)
   }
