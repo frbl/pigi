caf5dc626c8c8dda51645feb13d3dc6771d5a3d3
Frank Blaauw <frank.blaauw@gmail.com>
2012-09-27 12:49:29 +0200

    Graph and table are built according to the data in entries

--- pigi/src/main/scala/nl/rug/comet/UpdateStatistics.scala
+++ pigi/src/main/scala/nl/rug/comet/UpdateStatistics.scala
@@ -20,19 +20,35 @@ class Entry(pname: String, purl: String, pcomplexity: Int) {
 }
 
 class UpdateStatistics extends CometActor {
-  Schedule.schedule(this, Tick, 1 seconds)
+  Schedule.schedule(this, Tick, 3 seconds)
 	
 	val r = new Random()
 	
-	var entries = List(
-		new Entry("WebKit","svn://webkit.com/root", 3),
-		new Entry("wxWidgets","svn://wxWidgets.org/", 8),
-		new Entry("ReneZ","svn://assembla/sds", 92)
-	).zipWithIndex
-	
   def render = {
+		// Fake data from the database
+		val entries = List(
+			new Entry("WebKit","svn://webkit.com/root", r.nextInt(40)),
+			new Entry("wxWidgets","svn://wxWidgets.org/", r.nextInt(60)),
+			new Entry("ReneZ","svn://assembla/sds", r.nextInt(30)),
+			new Entry("Pigi","svn://pigid/hoi", r.nextInt(30))
+		).zipWithIndex
 	
-		val image = ("https://chart.googleapis.com/chart?cht=p3&chd=t:"+r.nextInt(40)+","+r.nextInt(60)+","+r.nextInt(30)+"&chs=250x100&chl=WebKit|wxWidgets|ReneZ");
+		// Create random complexities
+		entries.foreach(entry => {
+			println(entry._1.name)
+			entry._1.complexity = r.nextInt(40);
+		})
+		
+		// prepare the image url to include
+		//val image = ("https://chart.googleapis.com/chart?cht=p3&chd=t:"+entries(0)._1.complexity+","+
+		//																																entries(1)._1.complexity+","+
+		//																																entries(2)._1.complexity+
+		//																																"&chs=250x100&chl=" +
+		//																																entries(0)._1.name+"|"+
+		//																																entries(1)._1.name+"|"+
+		//																																entries(2)._1.name);
+		
+		val image = prepareImage(entries);
 		
 		".instance *" #> entries.map { case(entry, id) => {
         ".index *" #> id &
@@ -44,12 +60,31 @@ class UpdateStatistics extends CometActor {
 		"#time *" #> timeNow.toString &
 		"#chart [src]" #> image
 	}
+	
+	def prepareImage(entries:List[(Entry, Int)]):String = {
+		var imageurl = "https://chart.googleapis.com/chart?cht=p3&chd=t:";
+		
+		entries.foreach(x => {
+			imageurl += x._1.complexity + ","
+			}
+		)
+		
+		imageurl= imageurl.dropRight(1);
+		
+		imageurl +="&chs=250x100&chl=";
+		
+		entries.foreach(x => {
+			imageurl += x._1.name + "|"
+			}
+		)
+		return imageurl.dropRight(1);
+	}
 
   override def highPriority = {
     case Tick =>
 			println("tick");
 			reRender(true)
 			partialUpdate(SetHtml("time", Text(timeNow.toString)))
-      Schedule.schedule(this, Tick, 1 seconds)
+      Schedule.schedule(this, Tick, 3 seconds)
   }
 }
