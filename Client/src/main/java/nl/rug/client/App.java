4bbf346f89f190429a678853b583c6d803cdcbc8
Wes Schuitema <wes.schuitema@gmail.com>
2012-10-28 12:17:37 +0100

    changed the commandline parsing for some extra values that are needed

--- Client/src/main/java/nl/rug/client/App.java
+++ Client/src/main/java/nl/rug/client/App.java
@@ -33,31 +33,57 @@ public class App {
         // work. This is needed because of the Maven/JNI comination.
         SQLite.setLibraryPath("target/lib/");
 
-        if (args.length != 5) {
+        if (args.length != 7 || args.length != 5) {
 
-            logger.log(Level.SEVERE, "No repository specified. Usage: java -jar App.jar repository_address username password chord_seed_address chord_seed_port");
+            logger.log(Level.SEVERE, "No repository specified. Usage: java -jar App.jar repository_address username password address port [chord_seed_address chord_seed_port]");
 
             System.exit(1);
 
         }
         
-        //TODO Shouldnt the client recieve its own port to run on? or is this random or something?
         String repositoryAddress = args[0];
         String repositoryUsername = args[1];
         String repositoryPassword = args[2];
-        String chordSeedAddress = args[3];
-        int chordSeedPort = 0;
+        String address = args[3];
+        int port = 0;
         try {
             
-            chordSeedPort = Integer.parseInt(args[4]);
+            port = Integer.parseInt(args[4]);
             
         } catch (NumberFormatException ex) {
             
-            logger.log(Level.SEVERE, "Port specified should be a number.", ex);
+            logger.log(Level.SEVERE, "Port should be a number.", ex);
 
             System.exit(1);
             
         }
+        
+        // if we need to connect to an existing node
+        String chordSeedAddress = "";
+        int chordSeedPort = 0;
+        if (args.length == 7) {
+            
+            chordSeedAddress = args[5];
+            
+            try {
+            
+                logger.log(Level.INFO, "Chord seed port should be a number.");
+
+                chordSeedPort = Integer.parseInt(args[6]);
+
+            } catch (NumberFormatException ex) {
+
+                logger.log(Level.SEVERE, "Chord seed port should be a number.", ex);
+
+                System.exit(1);
+
+            }
+            
+            logger.log(Level.INFO, "Will connect to {0}:{1}", new Object[]{chordSeedAddress, chordSeedPort});
+            
+        }
+        
+        
 
         // Initialize the database, needed because I made everything too complex
         // and now we are stuck here initializing stuff for nothing
@@ -76,7 +102,7 @@ public class App {
         }
 
         // update the repository object (fetch missing and insert it into the database)        
-        RepositoryModel repositoryModel = new RepositoryModel(repositoryAddress, repositoryUsername, repositoryPassword);;
+        RepositoryModel repositoryModel = new RepositoryModel(repositoryAddress, repositoryUsername, repositoryPassword);
         try {
 
             repositoryModel.initialize();
@@ -90,11 +116,24 @@ public class App {
         }
 
         // load working set information (file/revision hashes and complexity values)
-        String nodeHash = Util.getHash(chordSeedAddress + chordSeedPort);
+        String nodeHash = Util.getHash(address + port);
         WorkingSet workingSet = new WorkingSet(repositoryAddress, nodeHash);
 
-
         // connect to the Chord network (fetch address from webservice and initilize network)
+        ClientController clientController = null;
+        
+        if (address.equals("") && port ==0) {
+            // this is the first node
+            clientController = new ClientController(port, workingSet);
+            
+        } else {
+            // connect to existing node
+            
+            
+        }
+          
+                
+                
 
         // start working (get next item from working set and retrieve from chord)
         int numberOfJobs = workingSet.getNumberOfJobs();
@@ -102,9 +141,6 @@ public class App {
         int remaining = numberOfJobs - finishedNumberOfJobs;
 
         logger.log(Level.INFO, "Starting work, {0} of {1} remaining.", new Object[]{remaining, numberOfJobs});
-
-        //TODO!!!!!!!!!! Chordseedport is not correct here!
-        ClientController clientController = new ClientController(chordSeedPort, workingSet);
 
         while (remaining != 0) {
 
