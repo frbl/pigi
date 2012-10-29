d96ca7190837e6747379fd282ca36d7598f67c72
Rene <renezuidhof@hotmail.com>
2012-10-29 05:23:03 +0100

    version changed

--- Client/src/main/java/nl/rug/client/controller/ClientController.java
+++ Client/src/main/java/nl/rug/client/controller/ClientController.java
@@ -104,7 +104,7 @@ public class ClientController {
      * @param args
      */
     public static void main(String args[]) {
-        Address localAddress = new Address("192.168.1.4", 4045);
+        Address localAddress = new Address("192.168.1.4", 4050);
         
         if(localAddress.getPort() != 4040) {
             Address remoteAddress = new Address("192.168.1.4", 4040);
